6daa62d7e4a9dde028546910c0a4bea5c976e8c8
Andrei Bituleanu <andrei.bituleanu@yahoo.com>
2026-07-14 20:28:37 -0400

    fixed

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	preview: {
-		allowedHosts: ["portfolio-production-a8b4.up.railway.app"],
+		allowedHosts: ["portfolio-star.up.railway.app"],
 	},
 });
